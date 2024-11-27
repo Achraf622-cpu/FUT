d17a92393afd0b2fe9ec0030b71a651a70e05123
Achraf <hanzazachraf581@gmail.com>
2024-11-27 23:17:50 +0100

    added a prototype to the image that should b posted in the wished place
    still has a lot os issues like the image isn't loading where i want it to be and stuff...

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const faceCards = document.querySelectorAll('.faceCard');
 const playersModal = document.getElementById('playersModal');
 const closeModal = document.getElementById('closeModal');
 const playersList = document.getElementById('playersList');
-
+let selectedPositionCard = null;
+let selectedPlayers = new Set();
 
 async function fetchPlayers() {
   try {
@@ -18,10 +19,11 @@ async function fetchPlayers() {
 async function showPlayers() {
     const playersData = await fetchPlayers();
     playersList.innerHTML = ''; 
-  
+    
+    playersData.players.filter(player => !selectedPlayers.has(player.name))
     playersData.players.forEach(player => {
       const playerCard = document.createElement('div');
-      playerCard.className = 'player-card';
+      playerCard.className = 'player-card cursor-pointer p-2 hover:bg-gray700 rounded';
   
       playerCard.innerHTML = `
         <div class="grid grid-cols-1 grid-rows-1">
@@ -30,15 +32,33 @@ async function showPlayers() {
           <h1 class="absolute font-bold z-10 top-2 left-2 ">${player.rating}</h1>
           <img src="${player.photo}" alt="Player Image" class="relative z-20 w-full h-full">
         </div>
-        <div">
+        <div>
           <h1>${player.name}</h1>
         </div>
+        </div>
       `;
-  
+      playerCard.addEventListener('click', () => {
+        assignPlayerToPosition(player);
+        playersModal.classList.add('hidden');
+      });
+
       playersList.appendChild(playerCard);
     });
   }
-  showPlayers();
+ 
+  function assignPlayerToPosition(player) {
+    if (!selectedPositionCard) return;
+
+    selectedPositionCard.innerHTML=`
+    <div class="relative flex flex-col items-center justify-center">
+       <img src="src/000.png" alt="" class="w-20 h-20 z-0 opacity-50 ">
+       <img src="${player.photo}" alt="" class="w-16 h-16 z-10 absolute mb-4">
+       <h1 class="text-center mt-1 text-sm font-semibold z-20 ">${player.name}</h1>
+    </div>
+    `;
+    selectedPlayers.add(player.name);
+    selectedPositionCard = null;
+  }
 
 
 faceCards.forEach(card => {
@@ -58,3 +78,5 @@ playersModal.addEventListener('click', (event) => {
     playersModal.classList.add('hidden'); 
   }
 });
+
+showPlayers();
